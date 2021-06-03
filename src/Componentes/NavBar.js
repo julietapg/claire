92d2e95715ac2930f3f8c353e3bb0555743d20a5
Julieta Pagano <julipagano@gmail.com>
2021-06-02 21:03:48 -0300

    nav

---
+++ src/Componentes/NavBar.js
@@ -0,0 +1,17 @@
+function App() {
+    return <>
+             
+             <NavBar />
+        							 
+                       <ul id="nav__ul">
+                        <li><a title="Claire" href="index.html"><img src="imagenes/logo.png" alt="logo" width="30%" height="30%" /></a></li>
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="tocados.html">TOCADOS</a></li>
+                        <li><a href="nosotros.html">NOSOTROS</a></li>
+                        <li><a href="cursos.html">CURSOS</a></li>
+                        <li> <a href="novias.html">NOVIAS</a></li>
+                        <li><a href="ramos.html">RAMOS</a></li>
+                      </ul>
+                   
+     </>;
+}
